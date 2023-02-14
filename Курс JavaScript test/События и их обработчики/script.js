7dbdbb6489faa2270d8ab3196dd0b96623d84961
BogdanovAD <bogdalexander@gmail.com>
2023-02-14 13:13:42 +0300

    add рекурсия

--- Курс JavaScript test/События и их обработчики/script.js
+++ Курс JavaScript test/События и их обработчики/script.js
@@ -1,46 +1,89 @@
 'use strict';
 
-const btn = document.querySelector('button'),
-    overlay = document.querySelector('.overlay');
+let students = {
+    js: [
+        {
+            name: 'John',
+            progress: 100,
+        },
+        {
+            name: 'Ivan',
+            progress: 60,
+        },
+    ],
 
-// btn.onclick = function () {
-//     alert('Click');
-// };
+    html: {
+        basic: [
+            {
+                name: 'Peter',
+                progress: 20,
+            },
+            {
+                name: 'Ann',
+                progress: 18,
+            },
+        ],
 
-// btn.addEventListener('click', () => {
-//     alert('Click');
-// });
+        pro: [
+            {
+                name: 'Sam',
+                progress: 10,
+            },
+        ],
+    },
+};
 
-// btn.addEventListener('mouseenter', (e) => {
-//     console.log(e.target);
-//     e.target.remove();
-// });
+function getTotalProgressByIteration(data) {
+    let total = 0,
+        students = 0;
 
-// let i = 0;
+    for (let course of Object.values(data)) {
+        if (Array.isArray(course)) {
+            students += course.length;
 
-function handler(e) {
-    console.log(e.currentTarget);
-    console.log(e.type);
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', handler);
-    // }
+            total += course.reduce((sum, current) => sum + current.progress, 0);
+
+            // for (let index = 0; index < course.length; index++) {
+            //     total += course[index].progress;
+            // }
+        } else {
+            for (let subCourse of Object.values(course)) {
+                students += subCourse.length;
+
+                total += subCourse.reduce((sum, current) => sum + current.progress, 0);
+
+                // for (let index = 0; index < subCourse.length; index++) {
+                //     total += subCourse[index].progress;
+                // }
+            }
+        }
+    }
+
+    return total / students;
 }
-btn.addEventListener('click', handler);
-overlay.addEventListener('click', handler);
 
-const link = document.querySelector('a');
+console.log(getTotalProgressByIteration(students));
 
-link.addEventListener('click', (e) => {
-    e.preventDefault(); // помещается в самое начало обработчика событий
-    console.log(e.target);
-});
+function getTotalProgressByRecursion(data) {
+    if (Array.isArray(data)) {
+        let total = 0;
 
-const btns = document.querySelectorAll('button');
+        total += data.reduce((sum, current) => sum + current.progress, 0);
 
-btns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        console.log(e.target);
-        console.log(e.type);
-    });
-});
+        return [total, data.length];
+    } else {
+        let total = [0, 0];
+
+        for (let subData of Object.values(data)) {
+            let subDataArr = getTotalProgressByRecursion(subData);
+            total[0] += subDataArr[0];
+            total[1] += subDataArr[1];
+        }
+
+        return total;
+    }
+}
+
+const result = getTotalProgressByRecursion(students);
+
+console.log(result[0] / result[1]);
